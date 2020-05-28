f7a9b3565e216d09d0c3c306c5b5caf3c1d6a3fb
Jorge <jorgediazok@gmail.com>
2020-05-28 03:43:21 -0300

    continue html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ang-todo';
+  title: string = 'Angular Crud';
+  employees: [
+    {
+      name: 'Mia Malkova';
+      position: 'manager';
+    },
+    {
+      name: 'Sasha Grey';
+      position: 'senior dv';
+    },
+    {
+      name: 'Anna Molly';
+      position: 'junior dv';
+    }
+  ];
+
+  model: any = {};
+
+  addEmployee(): void {}
+  deleteEmployee(): void {}
+  editEmployee(): void {}
+  updateEmployee(): void {}
 }
