9493c1aff5f5be28698f4a6ee2055fb7745c911a
Jorge <jorgediazok@gmail.com>
2020-05-28 21:12:24 -0300

    building all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -11,24 +12,40 @@ export class AppComponent {
     {
       name: 'Mia Malkova';
       position: 'manager';
+      email: 'email@email.com';
     },
     {
       name: 'Sasha Grey';
       position: 'senior dv';
+      email: 'email2@email.com';
     },
     {
       name: 'Anna Molly';
       position: 'junior dv';
+      email: 'email3@email.com';
     }
   ];
 
   model: any = {};
+  model2: any = {};
+  msg: string = '';
 
   addEmployee(): void {
     this.employees.push(this.model);
+    this.model = {};
+    this.msg = 'Record is succesfully Delete';
   }
 
   deleteEmployee(i): void {}
-  editEmployee(i): void {}
-  updateEmployee(): void {}
+
+  myValue;
+  editEmployee(i): void {
+    this.model2.name = this.employees[i].name;
+    this.model2.position = this.employees[i].position;
+    this.model2.email = this.employees[i].email;
+    this.myValue = i;
+  }
+  updateEmployee(): void {
+    console.log(this.model2);
+  }
 }
